c27220ee12d332f6e91b7e697116acfe1a58297e
abhis <abhishek.kalyanam@gmail.com>
2026-04-01 16:37:28 +0530

    commit 11

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/publicCourses.css";
 
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="navbar">
       <h1 className="logo">CourseFlow</h1>
-      <button className="login-btn">Admin Login</button>
+      <button
+        className="login-btn"
+        onClick={() => navigate("/Login")} 
+      >
+        Admin Login
+      </button>
     </div>
   );
 };
