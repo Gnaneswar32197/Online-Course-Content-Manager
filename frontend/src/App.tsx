029c368757ed5e7ae93e7057c1a3e5990113297a
abhis <abhishek.kalyanam@gmail.com>
2026-04-01 12:34:00 +0530

    login 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PublicCourses from "./pages/PublicCourses";
+import Login from "./pages/Login";
 
 function App() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
-
-  // Demo credentials
-  const demoEmail = "admin@gmail.com";
-  const demoPassword = "123456";
-
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (email === demoEmail && password === demoPassword) {
-      setMessage("Login Successful ✅");
-    } else {
-      setMessage("Invalid Credentials ❌");
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicCourses />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
